c8888728fa0ef62678c3bd42b2eaabe3ed826450
[dyad] <kiendt120702@gmail.com>
2025-08-15 09:07:14 +0700

    Reverted all changes back to version 8cf47e9c4d266e1f68d614664674589adb98c10d

--- src/pages/ComprehensiveReportsPage.tsx
+++ src/pages/ComprehensiveReportsPage.tsx
@@ -119,9 +119,9 @@ const ComprehensiveReportsPage = () => {
         <TabsList className="grid w-full grid-cols-3">
           <TabsTrigger value="reports">Báo cáo Doanh số</TabsTrigger>
           <TabsTrigger value="shop-management">Quản lý Shop</TabsTrigger>
-          <TabsTrigger value="user-management" className="flex items-center gap-2">
+          <TabsTrigger value="personnel-management" className="flex items-center gap-2">
             <Users className="h-4 w-4" />
-            Quản lý người dùng
+            Quản lý nhân sự
           </TabsTrigger>
         </TabsList>
         
@@ -246,7 +246,7 @@ const ComprehensiveReportsPage = () => {
           <ShopManagement />
         </TabsContent>
 
-        <TabsContent value="user-management">
+        <TabsContent value="personnel-management">
           <AdminUserManagement />
         </TabsContent>
       </Tabs>
