5155be6e16a3f4fc8d31f3b428813481eea915d3
[dyad] <kiendt120702@gmail.com>
2025-09-03 22:49:46 +0700

    [dyad] Hide shops without goals on dashboard - wrote 1 file(s)

---
+++ src/components/dashboard/GoalStatusDashboard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const GoalStatusDashboard: React.FC = () => {
+  // Per user request, this section is hidden from the dashboard.
+  return null;
+};
+
+export default GoalStatusDashboard;
