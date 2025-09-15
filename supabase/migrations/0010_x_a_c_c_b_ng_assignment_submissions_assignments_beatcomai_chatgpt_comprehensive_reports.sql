78797527549b1995c82c004412083e4658da2a6a
kiendt120702 <kiendt120702@gmail.com>
2025-09-15 16:16:45 +0700

    [dyad] Xóa 4 bảng khỏi cơ sở dữ liệu - wrote 1 file(s), executed 1 SQL queries

---
+++ supabase/migrations/0010_x_a_c_c_b_ng_assignment_submissions_assignments_beatcomai_chatgpt_comprehensive_reports.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS public.assignment_submissions;
+DROP TABLE IF EXISTS public.assignments;
+DROP TABLE IF EXISTS public.beatcomai_chatgpt;
+DROP TABLE IF EXISTS public.comprehensive_reports;
